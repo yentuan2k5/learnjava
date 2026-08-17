b47b22cb768716f499e69778033b7a0f99e7fc2a
Tô Nguyễn Tuấn Yên <yentuan2k5@gmail.com>
2026-08-17 11:19:01 +0700

    Bai-1

---
+++ Bai1/main.java
@@ -0,0 +1,14 @@
+package Bai1;
+
+public class main {
+    public static void main(String[] args) {
+        String name = "To Nguyen Tuan Yen";
+        int age = 21;
+        boolean isStudent = true;
+        double averageScore = 8.5;
+        System.out.println("Name is: " + name);
+        System.out.println("Age is: " + age);
+        System.out.println("Is student: " + isStudent);
+        System.out.println("Average score is: " + averageScore);
+    }
+}
